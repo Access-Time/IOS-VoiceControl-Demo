bd61a22d932d30b8a3bf6913f89c3e060eb693a5
Bogdan Sikora <bogdan@arokis.me>
2025-10-01 20:10:57 +0700

    Initial Commit

---
+++ VoiceControlDemo/VoiceControlDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceControlDemoApp.swift
+//  VoiceControlDemo
+//
+//  Created by Bogdan Sikora on 01.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct VoiceControlDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VoiceControlDemoTests/VoiceControlDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceControlDemoTests.swift
+//  VoiceControlDemoTests
+//
+//  Created by Bogdan Sikora on 01.10.2025.
+//
+
+import Testing
+@testable import VoiceControlDemo
+
+struct VoiceControlDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
